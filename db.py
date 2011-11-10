932a60cf97b18e01f85810aa617dd8c917d6f81b
Sebastian Henschel <sebastian@transparency.ge>
2011-11-10 16:45:52 +0400

    Modified database script to create hashes of documents and add them accordingly.

--- db.py
+++ db.py
@@ -1,9 +1,35 @@
+#!/usr/bin/env python
+"""
+Import income declaration reports into couchdb database.
+
+Usage:
+$ python db.py DIRECTORY
+
+DIRECTORY holds the reports as PDF + converted to HTML
+"""
+
+import os, sys, getopt, hashlib
 import couchdb
+from uuid import uuid4 # couchdb doc: uuids client-side, HTTP POST not idempotent
+
 import parse
 from incomeExceptions import BlankDeclarationError, MalformedDeclarationError
-import os, sys, getopt
 
-def main():
+
+
+# database configuration 
+DB = {
+    'url': 'http://127.0.0.1:5984',
+    'name': 'declarations',
+}
+
+
+
+def parse_opts ():
+    """Parse command-line options.
+    
+    @return list of directories to look for declarations to add
+    """
 # parse command line options
     try:
         opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
@@ -16,35 +42,122 @@ def main():
         if o in ("-h", "--help"):
             print __doc__
             sys.exit(0)
+
+    return args
+
+
+
+def get_db ():
+    """Get database object.
+
+    Looks in the global DB dictionary for the database server URL and the
+    database name.
+
+    @return database object
+    """
+    # don't catch exeptions, we want to crash here
+    couch = couchdb.client.Server(DB['url'])
+    return couch[DB['name']]
+
+
+
+def mkhash (html):
+    """Create a hash value for a PDF file.
+
+    @param html name of the HTML file corresponding to the hashed PDF
+    @return string message digest of the PDF file
+    """
+    h = hashlib.sha256()
+    pdf = html.split('.')[:-1]
+    pdf.append('pdf')
+    pdf = '.'.join(pdf)
+
+    try:
+        with open(pdf, 'r') as f:
+            for line in f:
+                h.update(line)
+    except IOError, e:
+        print "Couldn't find file to create hash: %s." % pdf
+        return None
+
+    return h.hexdigest()
+
+
+
+def add_to_db (db, doc):
+    """Add given document to database.
+
+    Or not if it already exxists. Also, be verbose about the action taken.
+
+    @param db database object
+    @param doc declaration document
+    @return boolean if document was added
+    """
+    view = db.view('basic/by_decl_id')
+    hits = view[doc['decl_id']]
+
+    if len(hits) > 0: # decl_id exists already
+        msg = "Duplicate declaration %s... " % doc['decl_id']
+
+        hashes = [db[h.id]['hash'] for h in hits if 'hash' in db[h.id]]
+        if doc['hash'] in hashes:
+            print msg + "Hash has not changed, skipping."
+            return False
+        else:
+            print msg + "New hash, adding new document."
+    else:
+        print "Adding declaration %s" % doc['decl_id']
+
+    uuid = uuid4().hex
+    db[uuid] = doc
+    return True
+
+
+
+def file_ok (filename):
+    """Check if given filename is a file we want to work with.
+
+    @param filename name of file to check
+    @return boolean if file is ok
+    """
+    if os.path.isdir(filename):
+        return False
+
+    suffix = filename.split('.')[-1]
+    if suffix not in ['html', 'htm']:
+        return False
+
+    return True
+
+
+
+def main ():
 # process arguments
+    args = parse_opts()
     home = os.getcwd()
-    couch = couchdb.client.Server("http://127.0.0.1:5984")
-    db = couch["declarations"]
-    tot_ct = 0
-    prs_ct = 0
-    add_ct = 0
+    db = get_db()
+    tot_ct = prs_ct = add_ct = 0
     for arg in args:
         files = os.listdir(arg)
         os.chdir(arg)
-        rows = db.view('basic/by_decl_id')
         for f in files:
+            if not file_ok(f): continue
+
             tot_ct += 1
             try:
-                uuid = couch.uuids()[0]
                 doc = parse.parse(f)
                 prs_ct += 1
-# Don't copy declarations we've already scraped.
-                if len(rows[doc["decl_id"]]) == 0:
-                    print "Adding declaration %s"%doc["decl_id"]
-                    db[uuid] = doc
-                    add_ct += 1
-                else:
-                    print "Duplicate declaration %s"%doc["decl_id"]
+                if doc:
+                    doc['hash'] = mkhash(f)
+                    if add_to_db(db, doc):
+                        add_ct += 1
             except MalformedDeclarationError:
-                print "Malformed Declaration: %s"%repr(f)
+                print "Malformed Declaration: %s" % repr(f)
         os.chdir(home)
 
-    print("Considered %d files. Parsed %d, added %d new files to db." %(tot_ct,prs_ct,add_ct))
+    print "Considered %d files. Successfully parsed %d, added %d new documents to db." % (tot_ct, prs_ct, add_ct)
+
+
 
 if __name__ == "__main__":
     main()
