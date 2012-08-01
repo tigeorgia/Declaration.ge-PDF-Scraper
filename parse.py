95fee3d71262bd7c4d0930ae0bf57fdd1af131d3
Derek Dohler <derek@transparency.ge>
2012-08-01 12:44:44 +0400

    Added GPL license

--- parse.py
+++ parse.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8
+
+#Asset declaration scraper -- scrapes and processes http://declaration.ge
+#Copyright 2012 Transparency International Georgia http://transparency.ge
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """Income declarations parser.
 
 Parses an income declaration from http://declaration.ge, converted
